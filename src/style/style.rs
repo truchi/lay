00c0f0309985be71f8900dd1f12bcd6865f01ac3
Romain TRUCHI <romain.truchi.06@gmail.com>
2020-09-14 13:13:52 +0200

    Redone Styler

--- src/style/style.rs
+++ src/style/style.rs
@@ -15,82 +15,80 @@ use std::fmt::{Display, Error, Formatter};
 /// `Option`al `Style`s.
 #[derive(Copy, Clone, Eq, PartialEq, Default, Debug)]
 pub struct Style {
-    pub foreground: Option<Foreground>,
-    pub background: Option<Background>,
-    pub weight:     Option<Weighted>,
-    pub slant:      Option<Slanted>,
-    pub blink:      Option<Blinking>,
-    pub invert:     Option<Inverted>,
-    pub strike:     Option<Striked>,
-    pub underline:  Option<Underlined>,
-    pub overline:   Option<Overlined>,
-    pub border:     Option<Bordered>,
+    pub foreground: Foreground,
+    pub background: Background,
+    pub weight:     Weighted,
+    pub slant:      Slanted,
+    pub blink:      Blinking,
+    pub invert:     Inverted,
+    pub strike:     Striked,
+    pub underline:  Underlined,
+    pub overline:   Overlined,
+    pub border:     Bordered,
 }
 
 impl Style {
-    /// Inherits fields from `parent`.
-    pub fn inherit(&self, parent: Self) -> Self {
-        macro_rules! inherit {
-            ($($field:ident)*) => {
-                Self {
-                    $($field: self.$field.or(parent.$field),)*
-                }
-            };
-        }
-        inherit!(foreground background weight slant blink invert strike underline overline border)
-    }
-
-    /// Unsets fields if the value is identical to the corresponding one in
-    /// `before`.
-    pub fn simplify(&self, before: Self) -> Self {
-        macro_rules! simplify {
-            ($($field:ident)*) => {
-                Self {
-                    $($field: match (before.$field, self.$field) {
-                        (Some(b), Some(s)) if b == s => None,
-                        _ => self.$field,
-                    },)*
-                }
-            };
-        }
-        simplify!(foreground background weight slant blink invert strike underline overline border)
-    }
-
-    /// Defaults (i.e. resets) fields which are set.
-    pub fn materialize(&self) -> Self {
-        macro_rules! materialize {
-            ($($field:ident)*) => {
-                Self {
-                    $($field: self.$field.or(Some(Default::default())),)*
-                }
-            };
-        }
-        materialize!(foreground background weight slant blink invert strike underline overline border)
-    }
-
-    /// Defaults (i.e. resets) fields which are set.
-    pub fn reset(&self) -> Self {
-        macro_rules! reset {
-            ($($field:ident)*) => {
-                Self {
-                    $($field: self.$field.and(Some(Default::default())),)*
-                }
-            };
-        }
-        reset!(foreground background weight slant blink invert strike underline overline border)
-    }
+    // /// Inherits fields from `parent`.
+    // pub fn inherit(&self, parent: Self) -> Self {
+    //     macro_rules! inherit {
+    //         ($($field:ident)*) => {
+    //             Self {
+    //                 $($field: self.$field.or(parent.$field),)*
+    //             }
+    //         };
+    //     }
+    //     inherit!(foreground background weight slant blink invert strike underline
+    // overline border) }
+    //
+    // /// Unsets fields if the value is identical to the corresponding one in
+    // /// `before`.
+    // pub fn simplify(&self, before: Self) -> Self {
+    //     macro_rules! simplify {
+    //         ($($field:ident)*) => {
+    //             Self {
+    //                 $($field: match (before.$field, self.$field) {
+    //                     (Some(b), Some(s)) if b == s => None,
+    //                     _ => self.$field,
+    //                 },)*
+    //             }
+    //         };
+    //     }
+    //     simplify!(foreground background weight slant blink invert strike
+    // underline overline border) }
+    //
+    // /// Defaults (i.e. resets) fields which are set.
+    // pub fn materialize(&self) -> Self {
+    //     macro_rules! materialize {
+    //         ($($field:ident)*) => {
+    //             Self {
+    //                 $($field: self.$field.or(Some(Default::default())),)*
+    //             }
+    //         };
+    //     }
+    //     materialize!(foreground background weight slant blink invert strike
+    // underline overline border) }
+    //
+    //   /// Defaults (i.e. resets) fields which are set.
+    //   pub fn reset(&self) -> Self {
+    //       macro_rules! reset {
+    //           ($($field:ident)*) => {
+    //               Self {
+    //                   $($field: self.$field.and(Some(Default::default())),)*
+    //               }
+    //           };
+    //       }
+    //       reset!(foreground background weight slant blink invert strike underline
+    // overline border)   }
 }
 
-impl_styler!(Style);
-impl_styler_ops!(Style);
+// impl_styler!(Style);
+// impl_styler_ops!(Style);
 
 impl Display for Style {
     fn fmt(&self, f: &mut Formatter) -> Result<(), Error> {
         macro_rules! display {
             ($($field:ident)*) => {
-                $(if let Some(field) = self.$field {
-                    write!(f, "{}", field)?;
-                })*
+                $(write!(f, "{}", self.$field)?;)*
             };
         }
 
--- src/style/styler.rs
+++ src/style/styler.rs
@@ -14,103 +14,82 @@ use super::{
 
 macro_rules! styler {
     (colors {
-        $($get_color:ident $get_color_mut:ident: $Color:ty {
+        $($get_color:ident $set_color:ident $set_color_mut:ident: $Color:ident {
             $set_rgb:ident $set_rgb_mut:ident $set_ansi:ident $set_ansi_mut:ident
             $($set_color_variant:ident $set_color_variant_mut:ident: $color_variant:ident)*
         })*
     }
     attributes {
-        $($get_attr:ident $get_attr_mut:ident: $Attr:ty {
-            $set_attr:ident $set_attr_mut:ident $unset_attr:ident $unset_attr_mut:ident
+        $($get_attr:ident $set_attr:ident $set_attr_mut:ident: $Attr:ty {
             $($set_attr_variant:ident $set_attr_variant_mut:ident : $attr_variant:ident)*
         })*
     }) => {
         /// A trait for styled types.
         pub trait Styler: Sized {
             $(
-                doc!("Returns a `&Option<" stringify!($Color) ">`",
-                    fn $get_color(&self) -> &Option<$Color>;
+                doc!("Gets `" stringify!($Color) "`",
+                    fn $get_color(&self) -> $Color;
                 );
-                doc!("Returns a `&mut Option<" stringify!($Color) ">`",
-                    fn $get_color_mut(&mut self) -> &mut Option<$Color>;
+                doc!("Sets `" stringify!($Color) "`",
+                    fn $set_color(self, color: $Color) -> Self;
                 );
+                doc!("Sets `" stringify!($Color) "`",
+                    fn $set_color_mut(&mut self, color: $Color);
+                );
+
                 doc!("Sets `" stringify!($Color) "(Color::Rbg { r, g, b })`",
-                    fn $set_rgb(mut self, r: u8, g: u8, b: u8) -> Self {
-                        self.$set_rgb_mut(r, g, b);
-                        self
+                    fn $set_rgb(self, r: u8, g: u8, b: u8) -> Self {
+                        self.$set_color($Color(Color::Rgb { r, g, b }))
                     }
                 );
                 doc!("Sets `" stringify!($Color) "(Color::Rbg { r, g, b })`",
                     fn $set_rgb_mut(&mut self, r: u8, g: u8, b: u8) {
-                        *self.$get_color_mut() = Some(Color::Rgb { r, g, b }.into());
+                        self.$set_color_mut($Color(Color::Rgb { r, g, b }));
                     }
                 );
                 doc!("Sets `" stringify!($Color) "(Color::AnsiValue(value))`",
-                    fn $set_ansi(mut self, value: u8) -> Self {
-                        self.$set_ansi_mut(value);
-                        self
+                    fn $set_ansi(self, value: u8) -> Self {
+                        self.$set_color($Color(Color::AnsiValue(value)))
                     }
                 );
                 doc!("Sets `" stringify!($Color) "(Color::AnsiValue(value))`",
                     fn $set_ansi_mut(&mut self, value: u8) {
-                        *self.$get_color_mut() = Some(Color::AnsiValue(value).into());
+                        self.$set_color_mut($Color(Color::AnsiValue(value)));
                     }
                 );
                 $(
                     doc!("Sets `" stringify!($Color) "(Color::" stringify!($color_variant) ")`",
-                        fn $set_color_variant(mut self) -> Self {
-                            self.$set_color_variant_mut();
-                            self
+                        fn $set_color_variant(self) -> Self {
+                            self.$set_color($Color(Color::$color_variant))
                         }
                     );
                     doc!("Sets `" stringify!($Color) "(Color::" stringify!($color_variant) ")`",
                         fn $set_color_variant_mut(&mut self) {
-                            *self.$get_color_mut() = Some(Color::$color_variant.into());
+                            self.$set_color_mut($Color(Color::$color_variant));
                         }
                     );
                 )*
             )*
             $(
-                doc!("Returns a `&Option<" stringify!($Attr) ">`",
-                    fn $get_attr(&self) -> &Option<$Attr>;
-                );
-                doc!("Returns a `&mut Option<" stringify!($Attr) ">`",
-                    fn $get_attr_mut(&mut self) -> &mut Option<$Attr>;
+                doc!("Gets `" stringify!($Attr) "`",
+                    fn $get_attr(&self) -> $Attr;
                 );
                 doc!("Sets `" stringify!($Attr) "`",
-                    fn $set_attr(mut self, attribute: $Attr) -> Self {
-                        self.$set_attr_mut(attribute);
-                        self
-                    }
+                    fn $set_attr(self, attribute: $Attr) -> Self;
                 );
                 doc!("Sets `" stringify!($Attr) "`",
-                    fn $set_attr_mut(&mut self, attribute: $Attr) {
-                        match attribute {
-                            $(<$Attr>::$attr_variant => self.$set_attr_variant_mut(),)*
-                        };
-                    }
+                    fn $set_attr_mut(&mut self, attribute: $Attr);
                 );
-                doc!("Unsets `" stringify!($Attr) "`",
-                    fn $unset_attr(mut self) -> Self {
-                        self.$unset_attr_mut();
-                        self
-                    }
-                );
-                doc!("Unsets `" stringify!($Attr) "`",
-                    fn $unset_attr_mut(&mut self) {
-                        *self.$get_attr_mut() = None;
-                    }
-                );
+
                 $(
                     doc!("Sets `" stringify!($Attr) "::" stringify!($attr_variant) "`",
-                        fn $set_attr_variant(mut self) -> Self {
-                            self.$set_attr_variant_mut();
-                            self
+                        fn $set_attr_variant(self) -> Self {
+                            self.$set_attr(<$Attr>::$attr_variant)
                         }
                     );
                     doc!("Sets `" stringify!($Attr) "::" stringify!($attr_variant) "`",
                         fn $set_attr_variant_mut(&mut self)  {
-                            *self.$get_attr_mut() = Some(<$Attr>::$attr_variant);
+                            self.$set_attr_mut(<$Attr>::$attr_variant);
                         }
                     );
                 )*
@@ -121,7 +100,7 @@ macro_rules! styler {
 
 styler!(
     colors {
-        get_foreground get_foreground_mut: Foreground {
+        get_foreground foreground foreground_mut: Foreground {
             rgb rgb_mut ansi ansi_mut
             white white_mut: White
             black black_mut: Black
@@ -138,7 +117,7 @@ styler!(
             cyan cyan_mut: Cyan
             dark_cyan dark_cyan_mut: DarkCyan
         }
-        get_background get_background_mut: Background {
+        get_background background background_mut: Background {
             on_rgb on_rgb_mut on_ansi on_ansi_mut
             on_white on_white_mut: White
             on_black on_black_mut: Black
@@ -157,45 +136,37 @@ styler!(
         }
     }
     attributes {
-        get_weight get_weight_mut: Weighted {
-            weighted weighted_mut no_weight no_weight_mut
+        get_weight weighted weighted_mut: Weighted {
             bold bold_mut: Bold
             light light_mut: Light
             reset_weight reset_weight_mut: ResetWeight
         }
-        get_slant get_slant_mut: Slanted {
-            slanted slanted_mut no_slant no_slant_mut
+        get_slant slanted slanted_mut: Slanted {
             italic italic_mut: Italic
             reset_slant reset_slant_mut: ResetSlant
         }
-        get_blink get_blink_mut: Blinking {
-            blinking blinking_mut no_blink no_blink_mut
+        get_blink blinking blinking_mut: Blinking {
             slow slow_mut: Slow
             fast fast_mut: Fast
             reset_blink reset_blink_mut: ResetBlink
         }
-        get_invert get_invert_mut: Inverted {
-            inverted inverted_mut no_invert no_invert_mut
+        get_invert inverted inverted_mut: Inverted {
             invert invert_mut: Invert
             reset_invert reset_invert_mut: ResetInvert
         }
-        get_strike get_strike_mut: Striked {
-            striked striked_mut no_strike no_strike_mut
+        get_strike striked striked_mut: Striked {
             strike strike_mut: Strike
             reset_strike reset_strike_mut: ResetStrike
         }
-        get_underline get_underline_mut: Underlined {
-            underlined underlined_mut no_underline no_underline_mut
+        get_underline underlined underlined_mut: Underlined {
             underline underline_mut: Underline
             reset_underline reset_underline_mut: ResetUnderline
         }
-        get_overline get_overline_mut: Overlined {
-            overlined overlined_mut no_overline no_overline_mut
+        get_overline overlined overlined_mut: Overlined {
             overline overline_mut: Overline
             reset_overline reset_overline_mut: ResetOverline
         }
-        get_border get_border_mut: Bordered {
-            bordered bordered_mut no_border no_border_mut
+        get_border bordered bordered_mut: Bordered {
             frame frame_mut: Frame
             circle circle_mut: Circle
             reset_border reset_border_mut: ResetBorder
