85116c48086522a8c20b0104d330362167a31c7d
Romain TRUCHI <romain.truchi.06@gmail.com>
2020-09-16 13:26:09 +0200

    Cell

--- src/style/cell.rs
+++ src/style/cell.rs
@@ -1,16 +1,11 @@
-use super::{Style, Styled};
+use super::Styled;
 
-/// `Styled<char>`.
+/// `Styled<Option<char>>`.
 ///
-/// A `Cell` conceptually has 2 layers:
-/// - a *foreground*: `Option<char>`, `Option<Foreground>` and other `Option`al
-///   text attributes (e.g. `Weighted`),
-/// - a *background*: `Option<Background>`.
-///
-/// With regards to colors, `None` denotes transparency.  
-/// With regards to content, `char::default()` (NUL) is the only char to denotes
-/// transparency. Hence, `' '` is a visible blank.
-pub type Cell = Styled<Option<char>, Style>;
+/// `Cell`s without `char` are invisible.  
+/// `Cell`s with `char` without `Foreground` have transparent foreground.  
+/// `Cell`s with `char` without `Background` have transparent background.
+pub type Cell = Styled<Option<char>>;
 
 /// See [`Cell`](type.Cell.html).
 impl Cell {
@@ -30,11 +25,6 @@ impl Cell {
     }
 
     /// Superimposes `above` above `self`.
-    ///
-    /// When `above` has a visible background, all we see is `above`.  
-    /// Otherwise when `above` has a visible foreground, we see `above` with
-    /// `below`'s background.  
-    /// Otherwise we see `below`.
     pub fn above(&self, above: &Self) -> Self {
         if above.has_content() {
             if above.has_background() {
@@ -48,26 +38,9 @@ impl Cell {
         } else {
             *self
         }
-        /*
-        if above.has_background() {
-            // Cannot see through `above`
-            return *above;
-        } else if above.has_foreground() {
-            // See through `above`'s backgroung
-            let mut above = *above;
-            above.style.background = self.style.background;
-
-            return above;
-        } else {
-            // `above` is invisible
-            *self
-        }
-        */
     }
 
     /// Superimposes `below` below `self`.
-    ///
-    /// See [`Cell::above()`](#method.above).
     pub fn below(&self, below: &Self) -> Self {
         below.above(self)
     }
