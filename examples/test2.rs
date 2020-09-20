c7c484839e84609d1658e5981694645fb1ac6ab8
Romain TRUCHI <romain.truchi.06@gmail.com>
2020-09-20 15:54:53 +0200

    Testing spaces

--- examples/test2.rs
+++ examples/test2.rs
@@ -1,5 +1,15 @@
+use crossterm::terminal::{size, Clear, ClearType};
 use lay::*;
 
 fn main() {
-    println!("Hello");
+    println!(":{}: {}", '\u{2003}', "EM");
+    println!(":{}: {}", '\u{3000}', "IDEOGRAPHIC");
+    println!(":🦀:");
+    println!(":{}: {}", '\u{2007}', "FIGURE");
+    println!(":{}: {}", '\u{2002}', "EN");
+    println!(":{}: {}", '\u{2008}', "PUNCTUATION");
+    println!(":{}: {}", '\u{2004}', "3 PER EM");
+    println!(":{}: {}", '\u{2005}', "4 PER EM");
+    println!(":{}: {}", '\u{2006}', "6 PER EM");
+    println!(":{}: {}", ' ', "NORMAL");
 }
