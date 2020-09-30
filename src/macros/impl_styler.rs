57625a9355b7c7b891345d304a9ad5321dbaaa11
Romain TRUCHI <romain.truchi.06@gmail.com>
2020-09-30 18:30:55 +0200

    impl_styler

--- src/macros/impl_styler.rs
+++ src/macros/impl_styler.rs
@@ -56,19 +56,26 @@ macro_rules! impl_styler {
         $border:expr,
     }) => {
         impl $(<$($G $(: $($B +)+)?,)+>)? $crate::Styler for $Type {
-            impl_styler!(impl [mode:$mode] Foreground get_foreground foreground_mut $self $foreground);
-            impl_styler!(impl [mode:$mode] Background get_background background_mut $self $background);
-            impl_styler!(impl [mode:$mode] Weighted get_weighted weighted_mut $self $weight);
-            impl_styler!(impl [mode:$mode] Slanted get_slanted slanted_mut $self $slant);
-            impl_styler!(impl [mode:$mode] Blinking get_blinking blinking_mut $self $blink);
-            impl_styler!(impl [mode:$mode] Inverted get_inverted inverted_mut $self $invert);
-            impl_styler!(impl [mode:$mode] Striked get_striked striked_mut $self $strike);
-            impl_styler!(impl [mode:$mode] Underlined get_underlined underlined_mut $self $underline);
-            impl_styler!(impl [mode:$mode] Overlined get_overlined overlined_mut $self $overline);
-            impl_styler!(impl [mode:$mode] Bordered get_bordered bordered_mut $self $border);
+            impl_styler!(impl [mode:$mode] $self {
+                Foreground get_foreground foreground_mut $foreground
+                Background get_background background_mut $background
+                Weighted   get_weighted   weighted_mut   $weight
+                Slanted    get_slanted    slanted_mut    $slant
+                Blinking   get_blinking   blinking_mut   $blink
+                Inverted   get_inverted   inverted_mut   $invert
+                Striked    get_striked    striked_mut    $strike
+                Underlined get_underlined underlined_mut $underline
+                Overlined  get_overlined  overlined_mut  $overline
+                Bordered   get_bordered   bordered_mut   $border
+            });
         }
 
         impl_styler!(impl [ops] $Type: $(<$($G $(: $($B)+)?,)+>)?);
+    };
+    (impl [mode:$mode:ident] $self:ident {
+        $($Attr:ident $get:ident $set_mut:ident $field:expr)*
+    }) => {
+        $(impl_styler!(impl [mode:$mode] $Attr $get $set_mut $self $field);)*
     };
     (impl [mode:field] $Attr:ident $get:ident $set_mut:ident $self:ident $field:expr) => {
         fn $get(&self) -> ::std::option::Option<$crate::$Attr> {
@@ -98,18 +105,30 @@ macro_rules! impl_styler {
     // ===
     (impl [ops] $Type:ty: $(<$($G:ident $(: $($B:path)+)?,)+>)?) => {
         impl_styler!(impl [ops:ops] $Type {
-            <$($($G $(: $($B)+)?,)+)?> Mul(mul) MulAssign(mul_assign) foreground_mut(foreground: Color NoColor) $crate::Foreground(foreground),
-            <$($($G $(: $($B)+)?,)+)?> Div(div) DivAssign(div_assign) background_mut(background: Color NoColor) $crate::Background(background),
-            <$($($G $(: $($B)+)?,)+)?> Add(add) AddAssign(add_assign) foreground_mut(foreground: Foreground NoForeground) foreground,
-            <$($($G $(: $($B)+)?,)+)?> Add(add) AddAssign(add_assign) background_mut(background: Background NoBackground) background,
-            <$($($G $(: $($B)+)?,)+)?> Add(add) AddAssign(add_assign) weighted_mut(weighted: Weighted NoWeight) weighted,
-            <$($($G $(: $($B)+)?,)+)?> Add(add) AddAssign(add_assign) slanted_mut(slanted: Slanted NoSlant) slanted,
-            <$($($G $(: $($B)+)?,)+)?> Add(add) AddAssign(add_assign) blinking_mut(blinking: Blinking NoBlink) blinking,
-            <$($($G $(: $($B)+)?,)+)?> Add(add) AddAssign(add_assign) inverted_mut(inverted: Inverted NoInvert) inverted,
-            <$($($G $(: $($B)+)?,)+)?> Add(add) AddAssign(add_assign) striked_mut(striked: Striked NoStrike) striked,
-            <$($($G $(: $($B)+)?,)+)?> Add(add) AddAssign(add_assign) underlined_mut(underlined: Underlined NoUnderline) underlined,
-            <$($($G $(: $($B)+)?,)+)?> Add(add) AddAssign(add_assign) overlined_mut(overlined: Overlined NoOverline) overlined,
-            <$($($G $(: $($B)+)?,)+)?> Add(add) AddAssign(add_assign) bordered_mut(bordered: Bordered NoBorder) bordered,
+            <$($($G $(: $($B)+)?,)+)?> Mul(mul) MulAssign(mul_assign)
+                foreground_mut(foreground: Color NoColor) $crate::Foreground(foreground),
+            <$($($G $(: $($B)+)?,)+)?> Div(div) DivAssign(div_assign)
+                background_mut(background: Color NoColor) $crate::Background(background),
+            <$($($G $(: $($B)+)?,)+)?> Add(add) AddAssign(add_assign)
+                foreground_mut(foreground: Foreground NoForeground) foreground,
+            <$($($G $(: $($B)+)?,)+)?> Add(add) AddAssign(add_assign)
+                background_mut(background: Background NoBackground) background,
+            <$($($G $(: $($B)+)?,)+)?> Add(add) AddAssign(add_assign)
+                weighted_mut(weighted: Weighted NoWeight) weighted,
+            <$($($G $(: $($B)+)?,)+)?> Add(add) AddAssign(add_assign)
+                slanted_mut(slanted: Slanted NoSlant) slanted,
+            <$($($G $(: $($B)+)?,)+)?> Add(add) AddAssign(add_assign)
+                blinking_mut(blinking: Blinking NoBlink) blinking,
+            <$($($G $(: $($B)+)?,)+)?> Add(add) AddAssign(add_assign)
+                inverted_mut(inverted: Inverted NoInvert) inverted,
+            <$($($G $(: $($B)+)?,)+)?> Add(add) AddAssign(add_assign)
+                striked_mut(striked: Striked NoStrike) striked,
+            <$($($G $(: $($B)+)?,)+)?> Add(add) AddAssign(add_assign)
+                underlined_mut(underlined: Underlined NoUnderline) underlined,
+            <$($($G $(: $($B)+)?,)+)?> Add(add) AddAssign(add_assign)
+                overlined_mut(overlined: Overlined NoOverline) overlined,
+            <$($($G $(: $($B)+)?,)+)?> Add(add) AddAssign(add_assign)
+                bordered_mut(bordered: Bordered NoBorder) bordered,
         });
         impl_styler!(impl [ops:bit] $Type {
             <$($($G $(: $($B)+)?,)+)?> BitAnd(bitand) BitAndAssign(bitand_assign) and_mut,
@@ -129,10 +148,10 @@ macro_rules! impl_styler {
         $fn:ident($rhs:tt: $Rhs:ident $NoRhs:ident) $body:expr,
     )* }) => {
         impl_styler!(impl trait binary $Type { $(
-            <$($G $(: $($B)+)?,)*>
-            $Op($op) $OpAssign($op_assign) $fn($rhs: $crate::$Rhs) Some($body),
-            <$($G $(: $($B)+)?,)*>
-            $Op($op) $OpAssign($op_assign) $fn(_: $crate::$NoRhs) None,
+            <$($G $(: $($B)+)?,)*> $Op($op) $OpAssign($op_assign)
+            $fn($rhs: $crate::$Rhs) Some($body),
+            <$($G $(: $($B)+)?,)*> $Op($op) $OpAssign($op_assign)
+            $fn(_: $crate::$NoRhs) None,
         )* });
     };
    (impl [ops:bit] $Type:ty { $(
