31346b2fad2e9222f10f74a554ce2eba70cb8682
Romain TRUCHI <romain.truchi.06@gmail.com>
2020-10-11 13:57:46 +0200

    Removed this nonsense

--- src/lib.rs
+++ src/lib.rs
@@ -66,43 +66,11 @@
 //! [impl_styler]: macro.impl_styler.html
 //! [gh_examples]: https://github.com/truchi/lay/tree/master/examples
 
-// #[macro_use]
-// mod macros;
-//
-// pub mod layer;
-// pub mod style;
-//
-// pub use layer::*;
-// pub use style::*;
+#[macro_use]
+mod macros;
 
-pub mod new {
-    use super::*;
-    use lay_macros::*;
+pub mod layer;
+pub mod style;
 
-    make_color!();
-    make_foreground!();
-    make_background!();
-    make_attrs!();
-    // make_style!();
-
-    /// LALALA  
-    /// ```
-    /// fn main() {
-    ///      let lol = "lol";
-    /// }
-    /// assert_eq!(12, 12, #prout);
-    /// ```
-    #[color(prout, lol = caca)]
-    _item! {}
-
-    test!(
-        /// LALALA
-        ///
-        /// ```
-        /// fn main() {
-        ///     let lol = "lol";
-        /// }
-        /// assert_eq!(12, 12, #prout);
-        /// ```
-    );
-}
+pub use layer::*;
+pub use style::*;
