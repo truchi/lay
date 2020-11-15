f831a480017277075d608c5e3e13a1f54aeb995c
Romain TRUCHI <romain.truchi.06@gmail.com>
2020-11-15 13:11:00 +0100

    (Gen: files)

--- build/generation/style/attributes.rs
+++ build/generation/style/attributes.rs
@@ -1,6 +1,28 @@
 use crate::generation::*;
 
 impl Generation {
+    pub fn mod_style_attributes(&self) -> TokenStream {
+        let doc = self
+            .0
+            .all
+            .iter()
+            .map(|attribute| format!("`{}`", attribute))
+            .collect::<Vec<_>>()
+            .join(", ");
+        let doc = idoc!("Attributes ({}).", doc);
+
+        let imports = self.0.all.iter().map(|attribute| {
+            let snake = &attribute.snake;
+            quote! { mod #snake; pub use #snake::*; #LINE_BREAK }
+        });
+
+        quote! {
+            #doc
+            #LINE_BREAK
+            #(#imports)*
+        }
+    }
+
     pub fn ground(&self, ground: &Attr) -> TokenStream {
         let color = &self.color;
         let color_snake = &color.snake;
@@ -52,6 +74,47 @@ impl Generation {
                 #from_color_option_doc
                 fn from(#color_snake: #color) -> Self {
                     Some(#ground(#color_snake))
+                }
+            }
+        }
+    }
+
+    pub fn attr(&self, attr: &Attr) -> TokenStream {
+        let variants = &attr.variants;
+        let reset = &attr.reset.full;
+
+        let decl_doc = doc!(
+            "`{attr}` ({variants}).
+
+            Prints the corresponding CSI to the terminal when `Display`ed.  
+            `Default`s to `{reset}`, the unsetting CSI.",
+            attr = attr,
+            reset = reset,
+            variants = variants
+                .iter()
+                .map(|variant| format!("`{}`", variant))
+                .collect::<Vec<_>>()
+                .join(", "),
+        );
+
+        let default_doc = doc!("Returns `{}`.", reset);
+
+        quote! {
+            pub use #attr::*;
+            #LINE_BREAK
+
+            #decl_doc
+            #[derive(Copy, Clone, Eq, PartialEq, Hash, Debug)]
+            pub enum #attr {
+                #(#variants,)*
+            }
+            #LINE_BREAK
+
+            #default_doc
+            impl Default for #attr {
+                #default_doc
+                fn default() -> Self {
+                    #reset
                 }
             }
         }
--- build/generation/style/mod.rs
+++ build/generation/style/mod.rs
@@ -1,33 +1,6 @@
-mod attribute;
+mod attributes;
 mod color;
-mod ground;
 mod markers;
 mod reset;
 mod style;
 mod styler;
-
-use crate::generation::*;
-
-impl Generation {
-    pub fn mod_style_attributes(&self) -> TokenStream {
-        let doc = self
-            .0
-            .all
-            .iter()
-            .map(|attribute| format!("`{}`", attribute))
-            .collect::<Vec<_>>()
-            .join(", ");
-        let doc = idoc!("Attributes ({}).", doc);
-
-        let imports = self.0.all.iter().map(|attribute| {
-            let snake = &attribute.snake;
-            quote! { mod #snake; pub use #snake::*; #LINE_BREAK }
-        });
-
-        quote! {
-            #doc
-            #LINE_BREAK
-            #(#imports)*
-        }
-    }
-}
