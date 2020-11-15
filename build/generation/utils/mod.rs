452e8e49bf555f73cc76914945d31e618948abf5
Romain TRUCHI <romain.truchi.06@gmail.com>
2020-11-15 13:14:26 +0100

    Gen: remove utils/ident

--- build/generation/utils/mod.rs
+++ build/generation/utils/mod.rs
@@ -1,14 +1,6 @@
-// macro_rules! ident {
-// ($fmt:literal) => { ::quote::format_ident!($fmt) };
-// ($fmt:expr) => { ::quote::format_ident!("{}", $fmt) };
-// ($fmt:expr, $($rest:tt)*) => { ::quote::format_ident!($fmt, $($rest)*) };
-// }
-
 #[macro_use]
 mod doc;
-// mod ident;
 mod write;
 
 pub use doc::*;
-// pub use ident::*;
 pub use write::*;
