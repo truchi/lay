4f91d06860e88fbc7dde7700c85a637346332e70
Romain TRUCHI <romain.truchi.06@gmail.com>
2020-11-18 12:39:31 +0100

    ()

--- build/generation/style/backends/mod.rs
+++ build/generation/style/backends/mod.rs
@@ -5,7 +5,7 @@ use crate::generation::*;
 impl Generation {
     pub fn mod_style_backends(&self) -> TokenStream {
         quote! {
-            // #[cfg(feature = "backend-crossterm")]
+            #[cfg(feature = "backend-crossterm")]
             mod crossterm;
         }
     }
