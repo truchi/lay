5e047cf0cb25905deccf483b67db62e28145415f
Romain TRUCHI <romain.truchi.06@gmail.com>
2020-11-19 08:36:13 +0100

    Gen: generate_style()

--- generation/src/main.rs
+++ generation/src/main.rs
@@ -82,119 +82,14 @@ impl Deref for Generation {
 
 fn main() {
     let cwd = std::env::current_dir().expect("Cannot get current directory");
-    let mut dir = cwd.parent().expect("Cannot get cwd's parent").to_path_buf();
-    dir.push("src/");
-    let dir = dir.to_str().expect("Cannot convert dir to str");
+    let dir = cwd.parent().expect("Cannot get cwd's parent");
+
     let gen = Generation(Lay::new());
     // panic!("{:#?}", gen);
 
-    // style/gen/attributes/
-    for ground in &gen.grounds {
-        write(
-            dir,
-            &format!("style/gen/attributes/{}.rs", ground.snake),
-            gen.ground(ground),
-        );
-    }
-    for attribute in &gen.attributes {
-        write(
-            dir,
-            &format!("style/gen/attributes/{}.rs", attribute.snake),
-            gen.attr(attribute),
-        );
-    }
-    write(
-        dir,
-        &format!("style/gen/attributes/mod.rs"),
-        gen.mod_style_attributes(),
-    );
-
-    // style/gen/backends/
-    write(
-        dir,
-        "style/gen/backends/crossterm.rs",
-        gen.backend_crossterm(),
-    );
-    write(dir, "style/gen/backends/mod.rs", quote! {
-        #[cfg(feature = "backend-crossterm")]
-        mod crossterm;
-    });
-
-    // style/gen/styler/
-    let (styler_index, styler_index_mut) = gen.styler_index();
-    let (styler, styler_mut) = gen.styler();
-    write(dir, "style/gen/styler/styler_index.rs", styler_index);
-    write(
-        dir,
-        "style/gen/styler/styler_index_mut.rs",
-        styler_index_mut,
-    );
-    write(dir, "style/gen/styler/styler.rs", styler);
-    write(dir, "style/gen/styler/styler_mut.rs", styler_mut);
-    write(dir, "style/gen/styler/mod.rs", gen.mod_style_styler());
-
-    // style/gen/
-    write(
-        dir,
-        &format!("style/gen/{}.rs", gen.color.snake),
-        gen.color(),
-    );
-    write(
-        dir,
-        &format!("style/gen/{}.rs", gen.reset.snake),
-        gen.reset(),
-    );
-    write(dir, &format!("style/gen/{}.rs", gen.index.snake), gen.i());
-    write(dir, &format!("style/gen/{}.rs", gen.none.snake), gen.no());
-    write(dir, "style/gen/style.rs", gen.style());
-    let styled_impls = (
-        &Str::new("Styled<T>"),
-        &[quote! { T: Display }][..],
-        &Str::new("style"),
-    );
-    write(
-        dir,
-        "style/gen/styled_impls.rs",
-        concat(&[
-            quote! {
-                use crate::*;
-                use std::fmt::Display;
-                // FIXME
-                use std::ops::Not;
-                #LINE_BREAK
-            },
-            gen.impl_styler_index(styled_impls),
-            gen.impl_styler_index_mut(styled_impls),
-            gen.impl_styler(styled_impls),
-            gen.impl_styler_mut(styled_impls),
-            gen.impl_styler_ops(styled_impls, false),
-            gen.impl_styler_ops(styled_impls, true),
-        ]),
-    );
-    write(
-        dir,
-        "style/gen/mod.rs",
-        concat(&[
-            quote! {
-                pub mod attributes;
-                mod backends;
-                mod color;
-                mod reset;
-                mod style;
-                mod styled_impls;
-                mod styler;
-                #LINE_BREAK
-
-                pub use attributes::*;
-                pub use color::*;
-                pub use reset::*;
-                pub use style::*;
-                pub use styler::*;
-                #LINE_BREAK
-            },
-            gen.import_markers(),
-        ]),
-    );
+    let mut style_dir = dir.to_path_buf();
+    style_dir.push("src/style/gen/");
+    gen.generate_style(style_dir.to_str().expect("Cannot convert dir to str"));
 }
 
 fn concat(streams: &[TokenStream]) -> TokenStream {
--- generation/src/style/mod.rs
+++ generation/src/style/mod.rs
@@ -5,3 +5,101 @@ mod markers;
 mod reset;
 mod style;
 mod styler;
+
+use crate::*;
+
+impl Generation {
+    pub fn generate_style(&self, dir: &str) {
+        // dir/attributes/
+        for ground in &self.grounds {
+            write(
+                dir,
+                &format!("attributes/{}.rs", ground.snake),
+                self.ground(ground),
+            );
+        }
+        for attribute in &self.attributes {
+            write(
+                dir,
+                &format!("attributes/{}.rs", attribute.snake),
+                self.attr(attribute),
+            );
+        }
+        write(
+            dir,
+            &format!("attributes/mod.rs"),
+            self.mod_style_attributes(),
+        );
+
+        // dir/backends/
+        write(dir, "backends/crossterm.rs", self.backend_crossterm());
+        write(dir, "backends/mod.rs", quote! {
+            #[cfg(feature = "backend-crossterm")]
+            mod crossterm;
+        });
+
+        // dir/styler/
+        let (styler_index, styler_index_mut) = self.styler_index();
+        let (styler, styler_mut) = self.styler();
+        write(dir, "styler/styler_index.rs", styler_index);
+        write(dir, "styler/styler_index_mut.rs", styler_index_mut);
+        write(dir, "styler/styler.rs", styler);
+        write(dir, "styler/styler_mut.rs", styler_mut);
+        write(dir, "styler/mod.rs", self.mod_style_styler());
+
+        // dir/
+        write(dir, &format!("{}.rs", self.color.snake), self.color());
+        write(dir, &format!("{}.rs", self.reset.snake), self.reset());
+        write(dir, &format!("{}.rs", self.index.snake), self.i());
+        write(dir, &format!("{}.rs", self.none.snake), self.no());
+        write(dir, "style.rs", self.style());
+        let styled_impls = (
+            &Str::new("Styled<T>"),
+            &[quote! { T: Display }][..],
+            &Str::new("style"),
+        );
+        write(
+            dir,
+            "styled_impls.rs",
+            concat(&[
+                quote! {
+                    use crate::*;
+                    use std::fmt::Display;
+                    // FIXME
+                    use std::ops::Not;
+                    #LINE_BREAK
+                },
+                self.impl_styler_index(styled_impls),
+                self.impl_styler_index_mut(styled_impls),
+                self.impl_styler(styled_impls),
+                self.impl_styler_mut(styled_impls),
+                self.impl_styler_ops(styled_impls, false),
+                self.impl_styler_ops(styled_impls, true),
+            ]),
+        );
+        write(
+            dir,
+            "mod.rs",
+            concat(&[
+                quote! {
+                    pub mod attributes;
+                    mod backends;
+                    mod color;
+                    mod reset;
+                    mod style;
+                    mod styled_impls;
+                    mod styler;
+                    #LINE_BREAK
+
+                    pub use attributes::*;
+                    pub use color::*;
+                    pub use reset::*;
+                    pub use style::*;
+                    pub use styler::*;
+                    #LINE_BREAK
+                },
+                self.import_markers(),
+            ]),
+        );
+    }
+}
