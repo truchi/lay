0e35ba25a4628aca5b700f86def591199669b4d8
Romain TRUCHI <romain.truchi.06@gmail.com>
2020-11-19 08:51:32 +0100

    Untrack generation/target...

--- generation/target/release/build/proc-macro2-242ed988b846770c/build_script_build-242ed988b846770c.d
+++
@@ -1,5 +0,0 @@
-/home/romain/perso/rust/lay/generation/target/release/build/proc-macro2-242ed988b846770c/build_script_build-242ed988b846770c: /home/romain/.cargo/registry/src/github.com-1ecc6299db9ec823/proc-macro2-1.0.24/build.rs
-
-/home/romain/perso/rust/lay/generation/target/release/build/proc-macro2-242ed988b846770c/build_script_build-242ed988b846770c.d: /home/romain/.cargo/registry/src/github.com-1ecc6299db9ec823/proc-macro2-1.0.24/build.rs
-
-/home/romain/.cargo/registry/src/github.com-1ecc6299db9ec823/proc-macro2-1.0.24/build.rs:
--- generation/target/release/deps/unicode_xid-ba23eaf40923c105.d
+++
@@ -1,8 +0,0 @@
-/home/romain/perso/rust/lay/generation/target/release/deps/unicode_xid-ba23eaf40923c105.rmeta: /home/romain/.cargo/registry/src/github.com-1ecc6299db9ec823/unicode-xid-0.2.1/src/lib.rs /home/romain/.cargo/registry/src/github.com-1ecc6299db9ec823/unicode-xid-0.2.1/src/tables.rs
-
-/home/romain/perso/rust/lay/generation/target/release/deps/libunicode_xid-ba23eaf40923c105.rlib: /home/romain/.cargo/registry/src/github.com-1ecc6299db9ec823/unicode-xid-0.2.1/src/lib.rs /home/romain/.cargo/registry/src/github.com-1ecc6299db9ec823/unicode-xid-0.2.1/src/tables.rs
-
-/home/romain/perso/rust/lay/generation/target/release/deps/unicode_xid-ba23eaf40923c105.d: /home/romain/.cargo/registry/src/github.com-1ecc6299db9ec823/unicode-xid-0.2.1/src/lib.rs /home/romain/.cargo/registry/src/github.com-1ecc6299db9ec823/unicode-xid-0.2.1/src/tables.rs
-
-/home/romain/.cargo/registry/src/github.com-1ecc6299db9ec823/unicode-xid-0.2.1/src/lib.rs:
-/home/romain/.cargo/registry/src/github.com-1ecc6299db9ec823/unicode-xid-0.2.1/src/tables.rs:
