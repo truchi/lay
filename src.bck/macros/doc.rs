3b816f791ae3dd4acbe7b416d862e5b14b662f86
Romain TRUCHI <romain.truchi.06@gmail.com>
2020-11-24 15:06:50 +0100

    Removed src.bck

--- src.bck/macros/doc.rs
+++
@@ -1,6 +0,0 @@
-#[doc(hidden)]
-#[macro_export]
-macro_rules! doc {
-    ($($doc:expr)*, $item:item) => { doc!{impl concat![" ", $($doc,)*], $item} };
-    (impl $doc:expr, $item:item) => { #[doc = $doc] $item };
-}
